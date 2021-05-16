a6688a9c5c1e7bd500b649590cb4b61c32ff0d2a
DavitBokuchava <davitbokuchava@gmail.com>
2021-05-16 23:26:10 +0400

    commit-37

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import cn from 'classnames';
 import st from './style.module.scss';
-// interface ButtonColor{
-//     color?: string
-// }
 
 interface ButtonProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  classname: string | null;
 }
-const Button: React.FC<ButtonProps> = ({ onClick }: ButtonProps) => {
+// children
+const Button: React.FC<ButtonProps> = ({ onClick, classname }: ButtonProps) => {
   return (
-    <button className={cn(st.root)} type="button" onClick={onClick}>
-      See pokemons
+    <button className={cn(st.root, classname)} type="button" onClick={onClick}>
+      {/* children */}
+      See Pokemons
     </button>
   );
 };
 export default Button;
+// there eslint called error:
+// error  propType "children" is not required, but has no corresponding defaultProps declaration  react/require-default-props
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,12 +5,31 @@ import Parallax from '../../components/Parallax';
 import Button from '../../components/Button';
 
 const HomePage = () => {
+  interface butns {
+    id: number;
+    button: string | null;
+    name?: any;
+  }
+
+  const buttons: butns[] = [
+    { id: 1, button: st.buttonOne, name: 'hi all' },
+    { id: 2, button: st.buttonTwo, name: 'hi all pokemons' },
+    { id: 3, button: st.buttonThree, name: 'hi all pokemons of the world ' },
+  ];
+
   return (
     <div className={st.root}>
       <Header />
       <div className={st.wrap}>
         <div className={st.left}>
-          <Button onClick={() => 'Click'} />
+          <div style={{ display: 'block' }}>
+            <h1>HI ALL </h1>
+            {buttons.map((el) => (
+              <Button key={el.id} onClick={() => 'Click'} classname={el.button}>
+                {/* See Pokemons */}
+              </Button>
+            ))}
+          </div>
         </div>
         <div className={st.right}>
           <Parallax />
