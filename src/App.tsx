b82f986d76e3c9c45a98959f5c574c1c0c9b647c
DavitBokuchava <davitbokuchava@gmail.com>
2021-05-22 20:35:56 +0400

    commit-59

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import { useRoutes } from 'hookrouter';
 import routes from './routes';
 import PageNotFound from './pages/PageNotFound';
 import Header from './components/Header';
-import req from './utils/request';
 /* eslint-disable */
 const App = () => {
-  req().then((data) => console.log(data.pokemons));
   const match = useRoutes(routes);
   console.log(match);
   return match ? (
--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -96,37 +96,39 @@ const Pokedex: React.FC<IPokedex> = ({ title }) => {
         {total} <b>Pokemons</b>
       </Heading>
       <div style={{ textAlign: 'center' }}>{title}</div>
-      {pokemons.map(
-        ({
-          name_clean,
-          abilities,
-          stats,
-          types,
-          img,
-          name,
-          base_experience,
-          height,
-          id,
-          is_default,
-          order,
-          weight,
-        }: IPokemon) => (
-          <PokemonCards
-            nameClean={name_clean}
-            abilities={abilities}
-            stats={stats}
-            types={types}
-            img={img}
-            name={name}
-            baseExperience={base_experience}
-            height={height}
-            id={id}
-            isDefault={is_default}
-            order={order}
-            weight={weight}
-          />
-        ),
-      )}
+      <div>
+        {pokemons.map(
+          ({
+            name_clean,
+            abilities,
+            stats,
+            types,
+            img,
+            name,
+            base_experience,
+            height,
+            id,
+            is_default,
+            order,
+            weight,
+          }: IPokemon) => (
+            <PokemonCards
+              nameClean={name_clean}
+              abilities={abilities}
+              stats={stats}
+              types={types}
+              img={img}
+              name={name}
+              baseExperience={base_experience}
+              height={height}
+              id={id}
+              isDefault={is_default}
+              order={order}
+              weight={weight}
+            />
+          ),
+        )}
+      </div>
       <div>
         <button onClick={() => setLimit((val) => val - 1)}>limit--</button>
         <button onClick={() => setLimit((val) => val + 1)}>limit++</button>
