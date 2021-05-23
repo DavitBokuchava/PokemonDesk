e5a148de1ba3591b37e9f89803ecd49ece7cc868
DavitBokuchava <davitbokuchava@gmail.com>
2021-05-24 03:43:05 +0400

    commit-501

--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -1,14 +1,28 @@
+/* eslint-disable */
 import config from '../config';
-// interface Iendpoint{
-//     getPokemons:Igetpokemons;
-// }
-// interface Igetpokemons{
-//     method:string;
-//     uri:Ipath;
-// }
-// interface Ipath{
-//     pathname:string;
-// }
+
+interface Iclient {
+  server: Iserver;
+  endpoint: Iendpoint;
+}
+interface Iserver {
+  protocol: string;
+  host: string;
+}
+interface Iendpoint {
+  getPokemons: Igetpokemons;
+}
+interface Igetpokemons {
+  method: string;
+  uri: Ipath;
+}
+interface Ipath {
+  pathname: string;
+}
+
+interface Iconfig {
+  client: Iclient;
+}
 
 function getUrlWithParamsConfig(endpontConfig: string) {
   const url = {
